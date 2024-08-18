babb13ed7b704edbde41492400653c90efd4a7b4
mzkm2843 <ff10mm11yy23tamk@gmail.com>
2024-08-18 15:15:30 +0900

    ユーザー認証機能の実装

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,30 @@
+# accounts/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import CustomUser
+from .forms import UserCreationForm
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    add_form = UserCreationForm
+
+    # リスト画面の表示項目とフィルター機能をもつ項目
+    list_display = ('email', 'username', 'is_staff', 'is_superuser')
+    list_filter = ('email', 'username','is_staff', 'is_superuser')
+
+    # ユーザー選択時に表示されるフィールド
+    fieldsets = (
+        ('ユーザー情報', {'fields': ('email', 'username')}),
+        ('権限', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
+
+    # 管理者画面でのユーザー作成時のフィールドを設定
+    add_fieldsets = (
+        ('ユーザー作成', {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email', 'username') # 検索するフィールド
+    filter_horizontal = ()
+
+admin.site.register(CustomUser, UserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError('メールアドレスを入力')
+        if not username:
+            raise ValueError('ユーザー名を入力')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, username, password=None):
+        user = self.create_user(email, username, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True, default='sample@example.com')
+    username = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    objects = CustomUserManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    
+    def __str__(self):
+        return self.email
