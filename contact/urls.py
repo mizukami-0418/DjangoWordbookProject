f104a71bea6d15a6735a9c563a0ef7dcbd11a23d
mzkm2843 <ff10mm11yy23tamk@gmail.com>
2024-08-25 15:10:10 +0900

    各htmlファイルの修正とuser_homeを新規に作成。Figmaデザインに準じて修正を施す

--- contact/urls.py
+++ contact/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.contact_view, name='contact'),
+    path('result/', views.contact_result, name='contact_result'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -8,14 +8,18 @@ from .forms import InquiryForm
 
 @login_required
 def contact_view(request):
+    success_message = 'お問い合わせを受け付けました。\n登録メールアドレスへ5営業日以内にご連絡いたします。'
     if request.method == 'POST':
         form = InquiryForm(request.POST)
         if form.is_valid():
             inquiry = form.save(commit=False)  # 一旦保存を延期
             inquiry.user = request.user  # ログインしているユーザーをセット
             inquiry.save()  # 保存
-            messages.success(request, 'お問い合わせを送信しました')
-            return redirect('detail')
+            messages.success(request, success_message)
+            return redirect('contact_result')
     else:
         form = InquiryForm()
     return render(request, 'contact/contact.html', {'form': form})
+
+def contact_result(request):
+    return render(request, 'contact/contact_result.html')
