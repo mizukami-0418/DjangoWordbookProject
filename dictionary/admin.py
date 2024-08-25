cc6729708fa7e98280f6e46b420ab18d8fcdcc60
mzkm2843 <ff10mm11yy23tamk@gmail.com>
2024-08-25 10:53:31 +0900

    Wordモデルの修正とadmin.pyの作成

--- dictionary/admin.py
+++ dictionary/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Word
 
-# Register your models here.
+
+class WordAdmin(admin.ModelAdmin):
+    list_display = ('english', 'japanese', 'part_of_speech', 'phrase', 'level')
+    list_filter = ('part_of_speech', 'level')
+    
+    fieldsets = (
+        ('language', {'fields': ('english', 'japanese')}),
+        ('description', {'fields': ('part_of_speech', 'phrase', 'level')}),
+    )
+    
+    search_fields = ('english', 'japanese')
+    
 admin.site.register(Word)
