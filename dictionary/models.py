12df8f4d4069d83ef8285062b5fb70083feaf5fa
mzkm2843 <ff10mm11yy23tamk@gmail.com>
2024-08-25 11:56:41 +0900

    admin.pyの修正とマイグレーションを実行

--- dictionary/models.py
+++ dictionary/models.py
@@ -9,25 +9,25 @@ class Word(models.Model):
     }
     
     PART_OF_SPEECH_SELECT = {
-        '名': '名詞',
-        '形': '形容詞',
-        '代': '代名詞',
-        '副': '副詞',
-        '動': '動詞',
-        '自': '自動詞',
-        '他': '他動詞',
-        '自他': '自動詞、他動詞',
-        '助': '助動詞',
-        '前': '前置詞',
-        '接': '接続詞',
-        '間': '間投詞',
+        '名詞': '名詞',
+        '形容詞': '形容詞',
+        '代名詞': '代名詞',
+        '副詞': '副詞',
+        '動詞': '動詞',
+        '自動詞': '自動詞',
+        '他動詞': '他動詞',
+        '自動詞・他動詞': '自動詞、他動詞',
+        '助動詞': '助動詞',
+        '前置詞': '前置詞',
+        '接続詞': '接続詞',
+        '間投詞': '間投詞',
     }
     
     english = models.CharField(max_length=255, verbose_name='英語') # 英語
     japanese = models.CharField(max_length=255, verbose_name='日本語') # 日本語
     part_of_speech = models.CharField(max_length=50, choices=PART_OF_SPEECH_SELECT, verbose_name='品詞') # 品詞
     phrase = models.TextField(blank=True, null=True, verbose_name='成句') # 成句や例文
-    level = models.CharField(max_length=20, choices=LEVEL_SELECT, verbose_name='難易度') # 難易度
+    level = models.CharField(max_length=20, choices=LEVEL_SELECT, verbose_name='難易度', null=True) # 難易度
     
     class Meta:
         db_table = 'word'
