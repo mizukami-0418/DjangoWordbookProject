ad5b2bbdd2069a25aee7251278799112ca49d0ee
mzkm2843 <ff10mm11yy23tamk@gmail.com>
2024-08-26 22:39:29 +0900

    パスワード変更機能の実装

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import CustomPasswordChangeView
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -9,4 +10,6 @@ urlpatterns = [
     path('user/', views.user_home, name='user_home'),
     path('user/edit/', views.user_edit, name='edit'),
     path('user/detail/', views.user_detail, name='detail'),
+    path('user/password_change/', CustomPasswordChangeView.as_view(), name='password_change'),
+    path('user/password_change/done', views.password_change_done, name='password_change_done'),
 ]
