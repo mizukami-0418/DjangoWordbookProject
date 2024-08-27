8406ab67b9b78b5ad6a4370a265d479506203e00
mzkm2843 <ff10mm11yy23tamk@gmail.com>
2024-08-27 16:55:20 +0900

    dictionaryモードの実装終了。

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.search, name='search'),
-    path('mode_choice', views.mode_choice, name='mode_choice'),
+    # path('mode_choice', views.mode_choice, name='mode_choice'), 一旦コメントアウト
     path('search_japanese', views.search_japanese, name='search_japanese'),
     path('search_english', views.search_english, name='search_english'),
 ]
