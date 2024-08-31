695e2e9a4ba1b1f85e20ec7672b6ca4deabc0e72
mzkm2843 <ff10mm11yy23tamk@gmail.com>
2024-08-31 18:41:17 +0900

    モード選択機能の作成

--- flashcard/urls.py
+++ flashcard/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.select_level, name='select_level'),
+    path('select_mode', views.select_mode, name='select_mode'),
 ]
--- flashcard/views.py
+++ flashcard/views.py
@@ -3,10 +3,33 @@ from dictionary.models import Word, Level
 from django.http import HttpResponse
 
 
+# 難易度セレクト
 def select_level(request):
     if request.method == 'POST':
         level_id = request.POST.get('level')
-        return HttpResponse(level_id)
+        
+        request.session['level_id'] = level_id
+        return redirect('select_mode')
     else:
+        # データベースからLevelオブジェクトを全て取得
         levels = Level.objects.all()
     return render(request, 'flashcard/select_level.html', {'levels': levels})
+
+
+# モードセレクト
+def select_mode(request):
+    # セッションからlevel_idを取得
+    level_id = request.session.get('level_id')
+    if not level_id:
+        return redirect('select_level')
+    
+    if request.method == 'POST':
+        mode = request.POST.get('mode')
+        
+        request.session['mode'] = mode
+        return HttpResponse(mode)
+    
+    # データベースからlevel_idに対応するLevelオブジェクトを取得
+    level = get_object_or_404(Level, id=level_id)
+    
+    return render(request, 'flashcard/select_mode.html', {'level': level})
