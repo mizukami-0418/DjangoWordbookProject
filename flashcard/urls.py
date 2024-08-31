c7b3544fb4f2c57194ac987645e9c1b5d3142ca1
mzkm2843 <ff10mm11yy23tamk@gmail.com>
2024-08-31 19:01:19 +0900

    問題数選択機能の追加

--- flashcard/urls.py
+++ flashcard/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.select_level, name='select_level'),
     path('select_mode', views.select_mode, name='select_mode'),
+    path('select_num_questions', views.select_num_questions, name='select_num_questions'),
 ]
--- flashcard/views.py
+++ flashcard/views.py
@@ -27,9 +27,27 @@ def select_mode(request):
         mode = request.POST.get('mode')
         
         request.session['mode'] = mode
-        return HttpResponse(mode)
+        return redirect('select_num_questions')
     
     # データベースからlevel_idに対応するLevelオブジェクトを取得
     level = get_object_or_404(Level, id=level_id)
     
     return render(request, 'flashcard/select_mode.html', {'level': level})
+
+
+# 問題数セレクト
+def select_num_questions(request):
+    level_id = request.session.get('level_id')
+    mode = request.session.get('mode')
+    if not level_id and mode:
+        return redirect('select_level')
+    
+    if request.method == 'POST':
+        num_questions = int(request.POST.get('num_questions'))
+        
+        request.session['num_questions'] = num_questions
+        return HttpResponse(num_questions)
+    
+    level = get_object_or_404(Level, id=level_id)
+    
+    return render(request, 'flashcard/select_num_questions.html', {'level': level, 'mode': mode})
