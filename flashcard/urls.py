55ba5b891e9c8f1bdb041f70b0b93d979a0286eb
mzkm2843 <ff10mm11yy23tamk@gmail.com>
2024-09-01 13:34:50 +0900

    単語帳クイズ機能の作成(1)

--- flashcard/urls.py
+++ flashcard/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.select_level, name='select_level'),
     path('select_mode', views.select_mode, name='select_mode'),
     path('select_num_questions', views.select_num_questions, name='select_num_questions'),
+    path('quiz', views.quiz, name='quiz'),
 ]
--- flashcard/views.py
+++ flashcard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from dictionary.models import Word, Level
 from django.http import HttpResponse
+import random
 
 
 # 難易度セレクト
@@ -46,8 +47,57 @@ def select_num_questions(request):
         num_questions = int(request.POST.get('num_questions'))
         
         request.session['num_questions'] = num_questions
-        return HttpResponse(num_questions)
+        return redirect('quiz')
     
     level = get_object_or_404(Level, id=level_id)
     
     return render(request, 'flashcard/select_num_questions.html', {'level': level, 'mode': mode})
+
+
+# 単語帳クイズ
+def quiz(request):
+    level_id = request.session.get('level_id')
+    mode = request.session.get('mode')
+    num_questions = request.session.get('num_questions')
+    level = get_object_or_404(Level, id=level_id)
+    
+    if not level_id and mode and num_questions:
+        return redirect('select_level')
+    
+    words = Word.objects.filter(level_id=level_id) # 選択した難易度の単語を抽出
+    # 抽出した単語が選択した出題数より少ない場合、エラーにならないための処理
+    total_questions = min(num_questions, words.count())
+    
+    # セッションに問題番号がない場合は新規で保存
+    if 'question_index' not in request.session:
+        # 抽出した単語の中から問題数分のidをランダムで取得
+        question_ids = random.sample(list(words.values_list('id', flat=True)), total_questions)
+        # 各データをセッションに保存
+        request.session['question_index'] = 0 # 最初の問題番号を0にする
+        request.session['score'] = 0 # 正解数を0に設定
+        request.session['question_ids'] = question_ids # 抽出したid
+        
+    # 現在の問題indexを取得
+    question_index = request.session['question_index']
+    
+    # 問題番号が問題数以上になれば全て終了
+    if question_index >= total_questions:
+        return HttpResponse('resultへ')
+    
+    # idから問題の単語を抽出する
+    question_id = request.session['question_ids'][question_index]
+    current_question = Word.objects.get(id=question_id)
+    
+    if request.method == 'POST':
+        # ユーザーの回答を両端の空白を削除し、小文字に変換する
+        answer = request.POST.get('answer').strip().lower()
+        correct_answer = current_question.english if mode == 'en' else current_question.japanese
+        if answer == correct_answer:
+            # 正解数を１加算
+            request.session['score'] += 1
+        # 問題番号を1加算
+        request.session['question_index'] += 1
+        
+        return redirect('quiz')
+
+    return render(request, 'flashcard/quiz.html', {'current_question': current_question, 'mode': mode, 'question_index': question_index, 'level': level})
