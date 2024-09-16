86ac2a186792da273f18c735d4322e4a0503a273
mzkm2843 <ff10mm11yy23tamk@gmail.com>
2024-09-16 11:56:09 +0900

    エラーハンドラー404と500を作成

--- wordbook/urls.py
+++ wordbook/urls.py
@@ -1,5 +1,11 @@
+from django.conf.urls import handler404, handler500
 from django.contrib import admin
 from django.urls import path, include
+from error import views
+
+# カスタムエラーハンドラ
+handler404 = 'error.views.custom_404'
+handler500 = 'error.views.custom_500'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
